2e429dc5f2c73905b59c61ccf47caa2a2f446393
ayrgrant <ayrgrant@gmail.com>
2015-06-23 16:26:58 +0300

    добавление обработчика события submit для формы choose-hotel

--- js/main.js
+++ js/main.js
@@ -1,35 +1,61 @@
 (function(){
     var chooseHotel = document.getElementById('choose-hotel');
 
-    var counters = chooseHotel.querySelectorAll('.counter');
+    if(chooseHotel) {
+        var counters = chooseHotel.querySelectorAll('.counter');
 
-    for(var i = 0, len= counters.length; i < len; i++) {
-        val = +counters[i].querySelector('.input-counter').value;
-        if(val === 0) {
-            counters[i].querySelector('.btn-counter.icon-minus')
-                .classList.add('btn-counter-disabled');
+        for(var i = 0, len= counters.length; i < len; i++) {
+            val = +counters[i].querySelector('.input-counter').value;
+            if(val === 0) {
+                counters[i].querySelector('.btn-counter.icon-minus')
+                    .classList.add('btn-counter-disabled');
+            }
         }
+
+        chooseHotel.addEventListener('click', function(event) {
+            target = event.target;
+
+            if(target.classList.contains('btn-toggle')) {
+                event.preventDefault();
+                chooseHotel.querySelector('.choose-hotel-popup')
+                    .classList.toggle('hidden');
+            }
+
+            if(target.classList.contains('icon-minus')) {
+                decNum(target.parentNode);
+                return;
+            }
+
+            if(target.classList.contains('icon-plus')) {
+                incNum(target.parentNode);
+                return;
+            }
+        });
+
+        chooseHotel.querySelector('form').addEventListener('submit', function(event) {
+            target = event.target;
+
+            requireds = chooseHotel.querySelectorAll('[required]');
+
+            for(var i = 0, len= requireds.length; i < len; i++) {
+                if(!isValid(requireds[i])) {
+                    event.preventDefault();
+
+                    requireds[i].classList.add('input-error');
+                    requireds[i].focus();
+                    requireds[i].oninput = function(e) {
+                        this.oninput = null;
+                        this.classList.remove('input-error');
+                    }
+
+                    alert('поле заполнено неверно');
+                    return;
+                }
+            }
+
+        });
     }
 
-    chooseHotel.addEventListener('click', function(event) {
-        target = event.target;
-
-        if(target.classList.contains('btn-toggle')) {
-            event.preventDefault();
-            chooseHotel.querySelector('.choose-hotel-popup')
-                .classList.toggle('hidden');
-        }
-
-        if(target.classList.contains('icon-minus')) {
-            decNum(target.parentNode);
-            return;
-        }
-
-        if(target.classList.contains('icon-plus')) {
-            incNum(target.parentNode);
-            return;
-        }
-    });
 
     function incNum(counter) {
         var input = counter.querySelector('.input-counter');
@@ -48,5 +74,15 @@
                 .classList.add('btn-counter-disabled');
         }
         input.value = +input.value - 1;
+    }
+
+    function isValid(input) {
+        console.log(input.type);
+        console.log(input.name);
+        console.log(input.value);
+        console.log(input.validity);
+
+        // ToDo: добавить полезную логику проверок
+        return input.value !== '';
     }
 })();
