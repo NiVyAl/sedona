178416117d7b8d5168bd92ec3cd45a146457a72f
ayrgrant <ayrgrant@gmail.com>
2015-06-25 15:17:25 +0300

    отладка обработки событий формы

--- js/main.js
+++ js/main.js
@@ -2,10 +2,12 @@
   var chooseHotel = document.getElementById('choose-hotel');
   var hotelsFilters = document.getElementById('hotels-filters');
 
+  // обработка событий для блока choose-hotel
   if(chooseHotel) {
     chooseHotel.addEventListener('click', function(event) {
       target = event.target;
 
+      // клик по кнопке показа/скрытия формы
       if(target.classList.contains('btn-toggle')) {
         event.preventDefault();
 
@@ -13,12 +15,13 @@
         popup.classList.toggle('popup-show');
         popup.classList.toggle('popup-hide');
 
-        if(!popup.classList.contains('hidden')) {
+        if(popup.classList.contains('popup-show')) {
           popup.querySelector('input').focus();
         }
         return;
       }
 
+      // клики по кнопкам [+]/[-] счетчика
       if(target.classList.contains('icon-minus')) {
         normalizeCounter(target.parentNode);
         decCounter(target.parentNode);
@@ -31,25 +34,29 @@
         return;
       }
 
+      // клик по кнопке выбора даты
       if(target.classList.contains('icon-calendar')) {
         setDate(target.parentNode);
         return;
       }
     });
 
+    // обработка глобального нажатия [ESC] для скрытия формы
     window.addEventListener('keydown', function(event) {
       target = event.target;
 
       if(event.keyCode === 27) {
         popup = chooseHotel.querySelector('.choose-hotel-popup');
 
-        if(!popup.classList.contains('hidden')) {
-          popup.classList.add('hidden');
+        if(popup.classList.contains('popup-show')) {
+          popup.classList.remove('popup-show');
+          popup.classList.add('popup-hide');
         }
         return;
       }
     });
 
+    // обработка события отправки формы
     chooseHotel.querySelector('form').addEventListener('submit', function(event) {
       target = event.target;
 
@@ -72,56 +79,50 @@
     });
   }
 
-
+  // обработка событий для блока hotels-filters
   if(hotelsFilters) {
 
   }
 
-
+  // очистка статуса ошибки для поля ввода
   function clearErrorState() {
     this.classList.remove('input-error');
     this.oninput = null;
   }
 
-
+  // коррекция ввода для счетчиков
   function normalizeCounter(counter) {
     var input = counter.querySelector('input');
 
     if(isCounterValid(input.value)) return;
 
     if(input.classList.contains('input-error')) {
-      (clearErrorState.bind(input))();
+      input.oninput();
     }
 
     input.value = 0;
-    counter.querySelector('.icon-minus').disabled = true;
   }
 
+  // базовые операции счетчика
   function incCounter(counter) {
     var input = counter.querySelector('input');
-    if(+input.value === 0) {
-      counter.querySelector('.icon-minus').disabled = false;
-    }
     input.value = +input.value + 1;
   }
 
   function decCounter(counter) {
     var input = counter.querySelector('input');
-    if(+input.value === 0) return;
-    if(+input.value === 1) {
-      counter.querySelector('.icon-minus').disabled = true;
-    }
+    if(+input.value <= 0) return;
     input.value = +input.value - 1;
   }
 
-
+  // базовые операции установки дат
   function setDate(elem) {
     // ToDo: добавить модуль установки даты
     alert('Заглушка для модуля установки даты');
     return true;
   }
 
-
+  // набор проверок коррекности ввода
   function isCounterValid(value) {
     return !isNaN(+value) && (value !== '') && (+value >= 0);
   }
@@ -130,6 +131,7 @@
     return (value !== ''); //ToDo: добавить полезную логику проверки
   }
 
+  // обобщающая функция проверки ввода
   function isValid(elem) {
     var input = elem.querySelector('input');
     console.log(input.type);
