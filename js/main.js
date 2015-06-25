16be7f046ab36c6f395282de88ae6aa70fa99acf
ayrgrant <ayrgrant@gmail.com>
2015-06-25 20:10:54 +0300

    реализация базовых реакций для ползунков фильтра диапазона цен

--- js/main.js
+++ js/main.js
@@ -4,8 +4,9 @@
 
   // обработка событий для блока choose-hotel
   if(chooseHotel) {
+    // обработка кликов по различным элементам
     chooseHotel.addEventListener('click', function(event) {
-      target = event.target;
+      var target = event.target;
 
       // клик по кнопке показа/скрытия формы
       if(target.classList.contains('btn-toggle')) {
@@ -43,7 +44,7 @@
 
     // обработка глобального нажатия [ESC] для скрытия формы
     window.addEventListener('keydown', function(event) {
-      target = event.target;
+      var target = event.target;
 
       if(event.keyCode === 27) {
         popup = chooseHotel.querySelector('.choose-hotel-popup');
@@ -58,7 +59,7 @@
 
     // обработка события отправки формы
     chooseHotel.querySelector('form').addEventListener('submit', function(event) {
-      target = event.target;
+      var target = event.target;
 
       var elems = chooseHotel.querySelectorAll('.input-date, .input-counter');
 
@@ -79,10 +80,73 @@
     });
   }
 
+
   // обработка событий для блока hotels-filters
   if(hotelsFilters) {
+    var range = hotelsFilters.querySelector('.cost-range');
+    var sliderMin = range.querySelector('.range-slider-min');
+    var sliderMax = range.querySelector('.range-slider-max');
 
+    // реализация перемещения ползунков
+    range.addEventListener('mousedown', function(event) {
+      var target = event.target;
+
+      var point1d = {};
+      var sliderCoords = {};
+
+      if(target === sliderMin || target === sliderMax) {
+        document.onmousemove = moveSlider(target);
+        document.onmouseup = stopSlider(target);
+
+        point1d.x = event.clientX;
+        point1d.dx = event.clientX - point1d.x;
+
+        sliderCoords.left = parseFloat(window.getComputedStyle(target).left);
+
+        if(target === sliderMin) {
+          sliderCoords.minLeft = 0;
+          sliderCoords.maxLeft = parseFloat(window.getComputedStyle(sliderMax).left);
+        } else {
+          sliderCoords.minLeft = parseFloat(window.getComputedStyle(sliderMin).left)
+          sliderCoords.maxLeft = target.parentElement.getBoundingClientRect().width;
+        }
+
+        console.log(event.type, point1d, sliderCoords);
+      }
+
+      function moveSlider(slider) {
+        return function(event) {
+          point1d.dx = event.clientX - point1d.x;
+          point1d.x = event.clientX;
+
+          if(sliderCoords.left === sliderCoords.maxLeft &&
+            point1d.x > slider.getBoundingClientRect().left) return;
+
+          if(sliderCoords.left === sliderCoords.minLeft &&
+            point1d.x < slider.getBoundingClientRect().left) return;
+
+          sliderCoords.left += point1d.dx;
+          if(sliderCoords.left > sliderCoords.maxLeft) sliderCoords.left = sliderCoords.maxLeft;
+          if(sliderCoords.left < sliderCoords.minLeft) sliderCoords.left = sliderCoords.minLeft;
+
+          slider.style.left = sliderCoords.left + 'px';
+
+          console.log(event.type, point1d, sliderCoords);
+        }
+      }
+
+      function stopSlider(slider) {
+        return function(event) {
+          console.log(event.type, event.clientX);
+          document.onmousemove = null;
+          document.onmouseup = null;
+          point1d = null;
+          sliderCoords = null;
+        }
+      }
+    });
   }
+
 
   // очистка статуса ошибки для поля ввода
   function clearErrorState() {
